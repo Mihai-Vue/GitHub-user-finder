6b2b84811310c91aac1a64c0d434abfd297d9b6c
Mihai Boureanu <boureanumihai@yahoo.com>
2023-03-17 01:21:40 +0200

    getting data form the GitHub public API

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Heloooooo');
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const userName = document.querySelector("input").value
+        const response = await fetch(`https://api.github.com/users/${userName}`)
+        const data = response.json()
+        console.log(data);
+    })
+})
+
+
+
+
+// github api:  https://api.github.com/users/<username>
